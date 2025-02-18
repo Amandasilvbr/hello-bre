d6537ee013b75b47f7dc562ac28e29edc6a565f8
BrenndaLandim <brennda.landim@monks.com>
2025-02-18 16:07:08 -0300

    ex1 solution

--- exercises/ex-1.go
+++ exercises/ex-1.go
@@ -1,1 +1,16 @@
 // 1- Escreva um programa em Go que imprima um triângulo de asteriscos de acordo com o número de linhas que o usuário desejar;
+
+package main
+
+import "fmt"
+
+func main(){
+	var num int
+	fmt.Scan(&num)
+	simb := "*"
+
+	for i:= 1; i <= num; i++ {
+		fmt.Println(simb)
+		simb = simb + "*" 
+	}
+}
